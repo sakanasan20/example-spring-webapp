49a674dd6e8a89c7c22839c44f265588799b7b38
Nick <nick.chen@cmp.com.tw>
2023-06-26 09:08:59 +0800

    Initial commit

---
+++ src/main/java/tw/niq/example/ExampleSpringWebappApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/niq/example/ExampleSpringWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.niq.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
